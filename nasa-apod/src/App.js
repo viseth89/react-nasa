6cd52cf78d1d37c590feb0490962005b2dcaa64d
viseth89 <viseth89@gmail.com>
2021-07-04 07:42:13 -0500

    Working on Home.js. Trying to understand how the urls are being linked and looking for a pattern to develop

--- nasa-apod/src/App.js
+++ nasa-apod/src/App.js
@@ -2,16 +2,16 @@ import React from 'react'
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from './components/Home'
 import NasaPhoto from "./components/NasaPhoto"
-
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Route component = {Home} path='/' exact />
+        <Route component = {NasaPhoto} path='/nasaphoto' exact />
+      </div>
+    </BrowserRouter>
   );
 }
 
