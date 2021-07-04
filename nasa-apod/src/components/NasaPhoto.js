3d437ccc6bc4cecf9c8e3231070570698d4f1583
viseth89 <viseth89@gmail.com>
2021-07-04 07:52:29 -0500

    At 19:47 mark, using async function. I noticed the function was created inside the route, in class we tend to build the function AFTER the export or return maybe I didn't catch that part properly

--- nasa-apod/src/components/NasaPhoto.js
+++ nasa-apod/src/components/NasaPhoto.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function NasaPhoto() {
+    const [photoData, setPhotoData] = useState(null);
+
+    useEffect(() => {
+        fetchPhoto();
+
+        // Should this function be created outside? 
+        // 19:47 Mark thinking this through
+        // Will continue to follow to learn more about her thought process.
+        async function fetchPhoto() {
+            const res = await fetch(
+                `https://api.nasa.gov/planetary/apod?api_key=Txbals3ag1fH0pPbwrcjdn4MnIZbYbdVDhCIn7qm`
+            )
+            const data = await res.json();
+            setPhotoData(data)
+        }
+    })
 
     return (
-        
+
     )
 }
