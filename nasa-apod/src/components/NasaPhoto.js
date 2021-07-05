6fd5678e9cf32c99f61644df21979fec65aaf87d
viseth89 <viseth89@gmail.com>
2021-07-05 11:51:19 -0500

    24 minute mark with photo, title and date

--- nasa-apod/src/components/NasaPhoto.js
+++ nasa-apod/src/components/NasaPhoto.js
@@ -1,33 +1,34 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
 export default function NasaPhoto() {
-    const [photoData, setPhotoData] = useState(null);
+  const [photoData, setPhotoData] = useState(null);
 
-    useEffect(() => {
-        fetchPhoto();
+  useEffect(() => {
+    fetchPhoto();
 
-        // Should this function be created outside? 
-        // 19:47 Mark thinking this through
-        // Will continue to follow to learn more about her thought process.
-        async function fetchPhoto() {
-            const res = await fetch(
-                `https://api.nasa.gov/planetary/apod?api_key=Txbals3ag1fH0pPbwrcjdn4MnIZbYbdVDhCIn7qm`
-            )
-            const data = await res.json();
-            setPhotoData(data)
-        }
-    }, [])
+    // Should this function be created outside?
+    // 19:47 Mark thinking this through
+    // Will continue to follow to learn more about her thought process.
+    async function fetchPhoto() {
+      const res = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=Txbals3ag1fH0pPbwrcjdn4MnIZbYbdVDhCIn7qm`
+      );
+      const data = await res.json();
+      setPhotoData(data);
+      console.log(data);
+    }
+  }, []);
 
-    if(!photoData) return <div />
+  if (!photoData) return <div />;
 
+  return (
+    <div>
+      <img src={photoData.url} alt={photoData.url} />
 
-    return (
-        <div>
-            <img 
-                src = {photoData.url}
-                alt = {photoData.url}
-            />
-        </div>
-    )
+      <h1>{photoData.title}</h1>
+      <p></p>
+      <p>{photoData.explanation}</p>
+    </div>
+  );
 }
